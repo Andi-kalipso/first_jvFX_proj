d4ed10a5d99e7c14922e8f5ed079bf64a5554362
Andrey Yanke <kalipsotime@gmail.com>
2024-04-17 21:50:06 +0300

    the second init commit for real proj

---
+++ src/main/java/com/example/first_jvfx_proj/Main.java
@@ -0,0 +1,33 @@
+package com.example.first_jvfx_proj;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+
+public class Main  extends Application {
+    @Override
+
+
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main-view.fxml"));     //вставляем сюда окно вида на fxml (css)
+        Scene scene = new Scene(fxmlLoader.load(), 1600, 900);                               //настройки сцены
+        stage.setResizable(false);                                                                 //ограничим размер окна для безопасного использования
+        stage.setTitle("The Main Window!");                                                        //название окна
+        stage.setScene(scene);                                                                     //передаем и показываем
+
+        stage.show();
+    }
+
+
+
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
+
+}
---
+++ src/main/java/com/example/first_jvfx_proj/ganeralTaskPage.java
@@ -0,0 +1,4 @@
+package com.example.first_jvfx_proj;
+
+public class ganeralTaskPage {
+}
