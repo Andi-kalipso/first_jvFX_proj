cc1a491c810b06a6aa8e5e57970368b6ed592966
Andrey Yanke <kalipsotime@gmail.com>
2024-04-17 19:22:13 +0300

    Initial commit

---
+++ src/main/java/com/example/first_jvfx_proj/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.first_jvfx_proj;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.first_jvfx_proj {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.first_jvfx_proj to javafx.fxml;
+    exports com.example.first_jvfx_proj;
+}
